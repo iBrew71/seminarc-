226099311dd9892859267e94bd4ea7d0d1c4ed73
«Andrew <rullya@inbox.ru>
2023-07-05 16:28:50 +0300

    kvadrat chisla

--- c#kvadratchisla/Program.cs
+++ c#kvadratchisla/Program.cs
@@ -1,2 +1,8 @@
-﻿int x = Convert.ToInt32(Console.ReadLine());
+﻿// Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное на само себя).
+
+// Например:
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
+int x = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine(x*x);
