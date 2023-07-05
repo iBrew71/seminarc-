97e47bb23b98ef221b5fc1262019a10c340c2dfd
«Andrew <rullya@inbox.ru>
2023-07-05 17:39:42 +0300

    add opredelenie poslednego 4isla

---
+++ c#poslednee_4islo/Program.cs
@@ -0,0 +1,9 @@
+﻿//  Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+
+Console.Clear();
+Console.Write("Enter number xxx: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Last number: {n % 10}");
