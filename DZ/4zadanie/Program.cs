0af6a1f6ec1782ff723ce88d0b51353ba9bc2188
«Andrew <rullya@inbox.ru>
2023-07-05 21:24:23 +0300

    last work

---
+++ DZ/4zadanie/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Clear();
+Console.Write("Enter number : ");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 2;
+if(N<2)
+   Console.Write($"Число не может быть меньше 2 "); 
+while(i <= N)
+{
+Console.Write($"{i} ");
+i = i + 2;
+}
