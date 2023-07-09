1564a686e60f541e040ae55a06dcaab49177c96f
«Andrew <rullya@inbox.ru>
2023-07-09 15:21:13 +0300

    123

--- c_sharp_seminar2/chetvert'/Program.cs
+++ c_sharp_seminar2/chetvert'/Program.cs
@@ -1,4 +1,4 @@
-﻿/ // Задача №21. Напишите программу, которая принимает на вход координаты двух 
+﻿// Задача №21. Напишите программу, которая принимает на вход координаты двух 
 // точек и находит расстояние между ними в 2D пространстве.
 
 // A (3,6); B (2,1) -> 5,09 
@@ -16,4 +16,4 @@ int x2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("y2 = ");
 int y2 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine(Math.Round(Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2)), 3))
+Console.WriteLine(Math.Round(Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2)), 3));
