e2285ea1544837651e6c2d6db5aecc4a6a5ec000
«Andrew <rullya@inbox.ru>
2023-07-09 14:54:05 +0300

    dlinna otrezka

---
+++ c_sharp_seminar2/dlinna_otrezka/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача №21. Напишите программу, которая принимает на вход координаты двух 
+// точек и находит расстояние между ними в 2D пространстве.
+
+// A (3,6); B (2,1) -> 5,09 
+// A (7,-5); B (1,-1) -> 7,21
+
+
+
+Console.Clear();
+Console.Write("x1 = ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("y1 = ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("x2 = ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("y2 = ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Math.Round(Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2)), 3));
