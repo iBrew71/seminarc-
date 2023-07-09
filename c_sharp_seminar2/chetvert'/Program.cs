28d05eaa245958620a0e8540f849f07181253485
«Andrew <rullya@inbox.ru>
2023-07-09 14:39:28 +0300

    naiti chetvert

---
+++ c_sharp_seminar2/chetvert'/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача №18. Напишите программу, которая по заданному номеру четверти, показывает
+// диапазон возможных координат точек в этой четверти (x и y).
+
+
+Console.Clear();
+Console.Write("Ведите номер четверти: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+while (n < 1 || n > 4)
+{
+    Console.Write("Ведите номер четверти: ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+
+if (n == 1)
+	Console.WriteLine("x > 0 and y > 0");
+else if (n == 2)
+	Console.WriteLine("x < 0 and y > 0");
+else if (n == 3)
+	Console.WriteLine("x < 0 and y < 0");
+else
+	Console.WriteLine("x > 0 and y < 0");
