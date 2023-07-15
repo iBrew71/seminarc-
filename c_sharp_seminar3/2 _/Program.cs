3beb7f86f73fa87abadc3b426bb5bfe701950337
«Andrew <rullya@inbox.ru>
2023-07-15 14:57:56 +0300

    ринимает на вход число N и выдаёт произведение чисел от 1 до N

---
+++ c_sharp_seminar3/2 _/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
+
+void f(int n)
+{
+    int result = 1;
+    for (int i = 2; i <= n; i ++)
+        result *= i; // result = result * i
+    Console.WriteLine(result);
+    
+}
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+f(n);
